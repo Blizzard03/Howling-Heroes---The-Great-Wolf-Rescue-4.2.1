6df3c7ed56454be9a63d99c4bb3fba1bf7729d01
Blizzard03 <99153189+Blizzard03@users.noreply.github.com>
2024-02-08 12:04:55 +0700

    remove character selector and resolve minor bug

--- Assets/Script/Controller/UI/Splash_Screen/Splash_Screen_Played.gd
+++ Assets/Script/Controller/UI/Splash_Screen/Splash_Screen_Played.gd
@@ -1,8 +1,7 @@
 extends Node
-
 func _ready():
 	$AnimationPlayer.play("Fade_In")
 	await get_tree().create_timer(6).timeout
 	$AnimationPlayer.play("Fade_Out")
 	await get_tree().create_timer(3).timeout
-	get_tree().change_scene_to_file("res://Assets/Scences/Splash/Splash_Screen.tscn")
+	get_tree().change_scene_to_file("res://Assets/Scences/UI/Splash/Splash_Screen.tscn")
